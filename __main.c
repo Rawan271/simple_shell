a6c25341abd916a081e65db717f9447f9319abac
Amna2017 <aiaashour2017@gmai.com>
2023-05-23 22:39:39 -0700

    main fun

--- __main.c
+++ __main.c
@@ -10,14 +10,14 @@
 int main(__attribute__((unused)) int argc, char **argv)
 {
 	char *input, **cmd;
-	int counter = 0, statue = 1, st = 0;
+	int cont = 0, statue = 1, st = 0;
 
 	if (argv[1] != NULL)
 		read_file(argv[1], argv);
 	signal(SIGINT, signal_to_handel);
 	while (statue)
 	{
-		counter++;
+		cont++;
 		if (isatty(STDIN_FILENO))
 			prompt();
 		input = _getline();
@@ -29,7 +29,7 @@ int main(__attribute__((unused)) int argc, char **argv)
 		cmd = parse_cmd(input);
 		if (_strcmp(cmd[0], "exit") == 0)
 		{
-			exit_bul(cmd, input, argv, counter);
+			exit_bul(cmd, input, argv, cont);
 		}
 		else if (check_builtin(cmd) == 0)
 		{
@@ -39,7 +39,7 @@ int main(__attribute__((unused)) int argc, char **argv)
 		}
 		else
 		{
-			st = check_cmd(cmd, input, counter, argv);
+			st = check_cmd(cmd, input, cont, argv);
 
 		}
 		free_all(cmd, input);
@@ -61,17 +61,17 @@ int check_builtin(char **cmd)
 		{"history", NULL},
 		{NULL, NULL}
 	};
-	int i = 0;
+	int ii = 0;
 		if (*cmd == NULL)
 	{
 		return (-1);
 	}
 
-	while ((fun + i)->command)
+	while ((fun + ii)->command)
 	{
-		if (_strcmp(cmd[0], (fun + i)->command) == 0)
+		if (_strcmp(cmd[0], (fun + ii)->command) == 0)
 			return (0);
-		i++;
+		ii++;
 	}
 	return (-1);
 }
@@ -82,9 +82,9 @@ int check_builtin(char **cmd)
  */
 void creat_envi(char **envi)
 {
-	int i;
+	int ii;
 
-	for (i = 0; environ[i]; i++)
-		envi[i] = _strdup(environ[i]);
-	envi[i] = NULL;
+	for (ii = 0; environ[ii]; ii++)
+		envi[ii] = _strdup(environ[ii]);
+	envi[ii] = NULL;
 }
